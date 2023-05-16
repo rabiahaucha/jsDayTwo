09f1f578b548a72f58a12b9537a1c36103f12c90
rabia <rabiahaucha9@gmail.com>
2023-05-16 14:31:33 +0200

    newwork

---
+++ script/code.js
@@ -0,0 +1,8 @@
+let subjects = [ 'Python' , 'C++' , 'VB.Net','Javascript' ,'Assembly' ];
+let lastElement = ('assembly')
+console.log(lastElement);
+// let middleElement = arry.pop();
+// console.log(middleElement);
+// console.log(arry[2]);
+let middle = subjects[2]
+console.log(middle);
